8a7e276b34d0bd9e166678ef3450e33ef32cde7f
LICLin95 <licanlin95@163.com>
2019-08-05 14:46:16 +0800

    update

---
+++ SDFInstallationScript.js
@@ -0,0 +1,12 @@
+/**
+ * @NApiVersion 2.0
+ * @NScriptType SDFInstallationScript
+ */
+define([], function() {
+    function run(context) {
+        // access to context.fromVersion and context.toVersion here
+    }
+    return {
+        run: run
+    };
+}); 
